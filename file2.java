5136e8372f9314107886d28872278712ec6a9a14
sunil <sunil_more@persistent.com>
2020-04-08 16:54:46 +0530

    ReqNo.:3 added comments

--- file2.java
+++ file2.java
@@ -1,8 +1,9 @@
+//abcd
 class HelloWorld 
 { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
-    public static void main(String args[]) 
+    public static void main(String args[123]) 
     { 
         System.out.println("Welcome to git"); 
     } 
